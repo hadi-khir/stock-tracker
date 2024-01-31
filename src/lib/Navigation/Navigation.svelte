8b654b59c5c6d0ddf43bc9437299ea95b8609380
Hadi Khir <hadi.khir@sap.com>
2024-01-31 20:19:55 +0100

    adding hamburger menu interactivity

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,29 +1,30 @@
 <nav class="list-nav p-4">
     <h3 class="p-2">Categories</h3>
-    <ul>
+    <hr />
+    <ul class="p-2">
         <li>
             <a href="/">
-                <span class="badge bg-primary-500">(icon)</span>
                 <span class="flex-auto">Top Stocks</span>
             </a>
+            <hr />
         </li>
         <li>
             <a href="/">
-                <span class="badge bg-primary-500">(icon)</span>
                 <span class="flex-auto">Top Growth Stocks</span>
             </a>
+            <hr />
         </li>
         <li>
             <a href="/">
-                <span class="badge bg-primary-500">(icon)</span>
                 <span class="flex-auto">Top Value Stocks</span>
             </a>
+            <hr />
         </li>
         <li>
             <a href="/">
-                <span class="badge bg-primary-500">(icon)</span>
                 <span class="flex-auto">Top Technology Stocks</span>
             </a>
+            <hr />
         </li>
         <!-- TODO: add github stuff -->
     </ul>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,23 @@
-<script>
-	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+<script lang='ts'>
+	import { AppShell, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
 	import '../app.pcss';
 	import Navigation from '$lib/Navigation/Navigation.svelte';
-	import GitHubIcon from '$lib/assets/github-mark.png';
+	import Shell from '$lib/Navigation/Shell.svelte';
+
+	initializeStores();
+	const drawerStore = getDrawerStore();
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
+<Drawer>
+	<Navigation />
+</Drawer>
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<div class="flex items-center">
-					<button class="btn btn-sm mr-4 lg:hidden">
-						<span>
-							<svg viewBox="0 0 100 80" class="fill-token h-4 w-4">
-								<rect width="100" height="20" />
-								<rect y="30" width="100" height="20" />
-								<rect y="60" width="100" height="20" />
-							</svg>
-						</span>
-					</button>
-					<strong class="text-xl uppercase">Stock Tracker</strong>
-				</div>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a target="_blank" rel="noopener noreferrer" href="https://github.com/hadi-khir/stock-tracker">
-					<img class="w-8" src={GitHubIcon} alt='GitHub Icon'/>
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<Shell on:click={drawerOpen} />
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
 		<Navigation />
